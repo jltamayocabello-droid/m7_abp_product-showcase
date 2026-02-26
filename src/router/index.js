7cf326d989370edd95604088b8dd8c7e0f891456
Jorge <jltamayocabello@gmail.com>
2026-02-26 10:33:12 -0300

    Se limpia el proyecto para dejar solo la base

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+history: createWebHistory(import.meta.env.BASE_URL),
+routes: [
+{
+path: '/',
+name: 'home',
+component: () => import('../views/HomeView.vue'),
+},
+],
 })
 
 export default router
