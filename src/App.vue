96119d102c4cc5c016e8c1e704a254de85d408c8
Jorge <jltamayocabello@gmail.com>
2026-02-27 17:57:17 -0300

    Se añade cantidad de productos en Home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="#">Product ShowCase</a>
+      <a class="navbar-brand" href="#"><strong>Product ShowCase</strong></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,48 @@
 
 <HeaderComp>Página Home</HeaderComp>
 
+<div class="card-quantity-products">
+      <p class="card-quantity-products__value">{{ productsStore.quantityProducts }}</p>
+      <p class="card-quantity-products__text">Cantidad productos</p>
+    </div>
+
 </div>
 </template>
 
 <script setup>
-import HeaderComp from '@/components/layouts/HeaderComp.vue';
+import HeaderComp from '@/components/layouts/HeaderComp.vue';import { useProductsStore } from '../stores/products.store';
+import { onMounted } from 'vue';
+
+const productsStore = useProductsStore()
+
+onMounted(async () => {
+  await productsStore.fetchProducts();
+});
+
 
 
 </script>
 
 <style lang="css" scoped>
+.card-quantity-products {
+  max-width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 2px solid black;
+  padding:1rem;
+  text-align: center;
+}
+
+.card-quantity-products__value {
+  font-size: 5rem;
+  font-weight: 700;
+}
+
+.card-quantity-products__text {
+  font-size: 3rem;
+  font-weight: 300;
+}
 
 </style>
